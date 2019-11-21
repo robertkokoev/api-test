8653b6425ec2cb30937ce42d6c2dd8007b77b767
robertkokoev <kokoev.robert@mail.ru>
2019-11-21 18:55:41 +0300

    Правки

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
-import { AbstractFilmsService } from './services/abstract-films.service';
-import { FilmsService } from './services/films.service';
+import { AbstractFilmsService } from './services//films/abstract-films.service';
+import { FilmsService } from './services/films/films.service';
 import { EllipsisPipe } from './pipes/ellipsis.pipe';
 import { FillPipe } from './pipes/fill.pipe';
 import { FormsModule } from '@angular/forms';
-import { DetailsService } from './services/details.service';
-import { AbstractDetailsService } from './services/abstract-details.service';
-import { GenresService } from './services/genres.service';
+import { DetailsService } from './services/details/details.service';
+import { AbstractDetailsService } from './services/details/abstract-details.service';
+import { GenresService } from './services/genres/genres.service';
 
 @NgModule({
   declarations: [
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractFilmsService, Film } from 'src/app/services/abstract-films.service';
-import { AbstractDetailsService, Details } from 'src/app/services/abstract-details.service';
-import { GenreAdapter, GenresService } from 'src/app/services/genres.service';
+import { AbstractFilmsService, Film } from 'src/app/services/films/abstract-films.service';
+import { AbstractDetailsService, Details } from 'src/app/services/details/abstract-details.service';
+import { GenreAdapter, GenresService } from 'src/app/services/genres/genres.service';
 
 
 @Component({
@@ -13,9 +13,9 @@ export class MainComponent implements OnInit {
 
   details: Details;
   detailsGenres: string[] = [];
-  countres: string[] = [];
+  countries: string[] = [];
   query: string;
-  films: Film[] = [];
+  films: Film[];
   totalPages: number;
   page: number;
   pagesArr: number [] = [];
@@ -32,7 +32,7 @@ export class MainComponent implements OnInit {
   ngOnInit() {
     this.genresService.getGenres().subscribe(data => {
       this.genres = data;
-    })
+    });
   }
 
   onSearchClick(page: number) {
@@ -50,25 +50,31 @@ export class MainComponent implements OnInit {
     })
   }
 
-  onDetails(id: number) {
+  showDetails(id: number) {
     this.detailsService.getDetails(id).subscribe(data => {
       this.details = data;
       this.detailsGenres = data.genres
-      this.countres = data.productionCountries;
+      this.countries = data.productionCountries;
     })
   }
 
+  current: number;
+
   fill(number: number) {
-    let current = this.page;
+    this.current = this.page;
 
     for (let i = 0; i < number; i++) {
       this.arr.push(i);  
     }
 
-    if (current > 3) {
-      this.new = this.arr.slice(current - 4, current + 3);
-    } else {
-      this.new = this.arr.slice(0, current + 5);
+    if ((this.totalPages > 3) && (this.current > 2) && (this.current <= this.totalPages - 3)) {
+      this.new = this.arr.slice(this.current - 3, this.current + 2);
+    } else if ((this.totalPages < 3) && (this.current < 2)) {
+      this.new = this.arr.slice(0, this.totalPages);
+    } else if ((this.totalPages > 5) && (this.current < 4)) {
+      this.new = this.arr.slice(0, 5);
+    } else if ((this.totalPages > 3) && (this.current >= this.totalPages - 4)) {
+      this.new = this.arr.slice(this.totalPages - 4, this.totalPages);
     }
     return this.new; 
   }
